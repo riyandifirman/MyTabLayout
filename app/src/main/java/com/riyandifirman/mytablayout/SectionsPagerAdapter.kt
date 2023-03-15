90c1c9396d2acef6cd52ced97eb1089e55b930e4
riyandifirman <riyandi.firman08@gmail.com>
2023-03-15 12:46:22 +0700

    change configuration to tab layout with one fragment

--- app/src/main/java/com/riyandifirman/mytablayout/SectionsPagerAdapter.kt
+++ app/src/main/java/com/riyandifirman/mytablayout/SectionsPagerAdapter.kt
@@ -1,21 +1,21 @@
 package com.riyandifirman.mytablayout
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
+        val fragment = HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
         }
-        return fragment as Fragment
+        return fragment
     }
 
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 
 }
